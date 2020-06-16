4a262b9a8f14f52d878676e6776368c38b221080
aleksandrvayt <53118312+aleksandrvayt@users.noreply.github.com>
2020-06-16 13:27:49 +0300

    test-task

---
+++ t/js/main.js
@@ -0,0 +1,10 @@
+
+	// currentphoto = this.photo;
+$('.main__select').on('click',function () {
+	let thisi = $(this),
+	let photo = $('.foto'),
+	console.log(thisi.photo)
+	// currentphoto.html('hello')
+
+
+})
